cb527ddc5c59806705cabe8462b86947b333cebc
Willy <will.h.donaldson@gmail.com>
2017-10-11 22:09:17 -0700

    unsure tf just changed but testing linters

--- basic.py
+++ basic.py
@@ -4,7 +4,7 @@ docstring
 
 import matplotlib.pyplot as plt
 import numpy as np
-x=np.linspace(0, 2 * np.pi, 1000)
+x = np.linspace(0, 2 * np.pi, 1000)
 y = np.sin(x)
 plt.plot(x, np.sin(x))
 plt.show()
